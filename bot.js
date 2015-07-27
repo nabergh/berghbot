18a1ffe84970cfc435d348a3daaa5dc8bd8de64e
Nicholas Bergh <nicholasabergh@gmail.com>
2015-07-27 17:47:18 -0400

    test

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
     botRegex = /^\/cool guy$/;
-  console.log(JSON.stringify(request));
+  console.log("User id: " + request["user_id"]);
 
   if (request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
